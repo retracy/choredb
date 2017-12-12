d5080cb45a417779b804ebf5b4de1777592e2932
Bob Tracy <btracy@tektronix.com>
2017-12-12 07:56:13 -0800

    Clean up lint error

--- src/api/baseUrl.js
+++ src/api/baseUrl.js
@@ -1,5 +1,4 @@
 export default function getBaseUrl() {
     const inDevelopment = window.location.hostname == 'localhost';
-    console.log(`inDevelopment = ${inDevelopment}`);
     return inDevelopment ? 'http://localhost:3001/' : '/';
 }
